b15aacdf615b4ea0bf254f9c18a381d01ec1219e
Mary <marieshix98@gmail.com>
2023-06-20 23:49:40 +0300

    change form layout

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -23,18 +23,20 @@ const AddTask = () => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="w-[35%] mt-24 m-auto bg-white p-5 rounded-[10px] shadow-lg shadow-[rgba(0, 0, 0, 0.25)] w-full lg:max-w-[900px]"
+      className="w-[40%] mt-16 m-auto bg-white p-5 rounded-[10px] shadow-lg shadow-[rgba(0, 0, 0, 0.25)] w-full lg:max-w-[900px]"
     >
         <div>
             <Title title="Add Task"/>
         </div>
-      <div className="my-2">
+      <div className="flex justify-between gap-8">
+      <div className="my-2 w-[40%] ">
         <CustomLabel>Task</CustomLabel>
         <CustomInput name="task" value={task} onChange={handleChange} type="text"/>
       </div>
-      <div className="my-2">
+      <div className="my-2 w-[60%]">
         <CustomLabel>Description</CustomLabel>
-        <CustomTextArea name="task" value={desc} onChange={handleChange} placeholder="Write a description..."/>
+        <CustomTextArea row={1} name="task" value={desc} onChange={handleChange} placeholder="Write a description..."/>
+      </div>
       </div>
       <div className="my-2">
         <CustomButton>Submit</CustomButton>
