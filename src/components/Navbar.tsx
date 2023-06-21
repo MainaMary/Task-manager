738cac0caceac4bf1371ed2229fcd13393b1190b
Mary <marieshix98@gmail.com>
2023-06-21 13:10:00 +0300

    show the number of tasks on the nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
 import {IoMdNotifications} from "react-icons/io"
+import { useTaskList } from "../context/appContext"
 const Navbar = () => {
+  const {allTasks} = useTaskList()
   return (
     <div className="lg:h-24 lg:px-16 flex justify-between h-auto items-center bg-primary-color text-white text-md">
       <h4>Task Manager</h4>
+      <div className="relative">
       <IoMdNotifications size={32}/>
+      {allTasks.length > 0 ?  <p>{allTasks.length}</p> :''}
+     
+      </div>
+      
     </div>
   )
 }
