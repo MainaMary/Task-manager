26eb5a78abd4ae7d864f90ac9a5bb6232a150ab7
Mary <marieshix98@gmail.com>
2023-06-22 23:23:29 +0300

    fix custom text area

--- src/components/CustomTextArea.tsx
+++ src/components/CustomTextArea.tsx
@@ -2,7 +2,8 @@ import { TextAreaProps } from "../model/types"
 
 const CustomTextArea = ({name, placeholder, value, onChange, row}:TextAreaProps) => {
   return (
-    <textarea name={name} onChange={onChange}  value={value} id="editor" rows={row} className=" bg-gray-50 border border-gray-300 text-gray-900  rounded-sm block w-full text-sm  dark:text-white dark:placeholder-gray-400 focus:ring-blue-500 focus:border-blue-500  p-2.5" placeholder={placeholder} ></textarea>
+    <textarea name={name} onChange={onChange}  value={value} id="editor" rows={row} 
+    className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none" placeholder={placeholder} ></textarea>
   )
 }
 
