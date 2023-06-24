38fdfc22c65c81472f2fa71664b6aaa527c1cc55
Mary <marieshix98@gmail.com>
2023-06-24 23:59:53 +0300

    update component naming convection

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -8,76 +8,76 @@ interface initialStateType {
     setAllTasks: any,
     isComplete: boolean,
     setIsComplete: any,
-    setEditTask:any,
-    handleDelete:(x:string) =>void,
-    clearList: () =>void,
-    findTask:(x:string) =>void,
-    editTask:{
-        id:string,
+    setEditTask: any,
+    handleDelete: (x: string) => void,
+    clearList: () => void,
+    findTask: (x: string) => void,
+    editTask: {
+        id: string,
         time: string,
-        desc:string,
-        task:string
+        desc: string,
+        task: string
     },
-    handleEdit: (x:TaskType) =>void
+    handleEdit: (x: TaskType) => void
 
 }
 
 const TaskList = createContext<
-initialStateType
+    initialStateType
 >({
     allTasks: [],
     isComplete: false,
-    setAllTasks:[] ,
-    setEditTask:{},
-    setIsComplete:[],
-    handleDelete: () =>{},
-    clearList: () =>{},
-    findTask: () =>{},
+    setAllTasks: [],
+    setEditTask: {},
+    setIsComplete: [],
+    handleDelete: () => { },
+    clearList: () => { },
+    findTask: () => { },
     editTask: {
-        id:'',
-        time:'',
-        desc:'',
-        task:''
+        id: '',
+        time: '',
+        desc: '',
+        task: ''
     },
-    handleEdit : () =>{}
+    handleEdit: () => { }
 })
-export const useTaskList = () =>{
+export const useTaskList = () => {
     return useContext(TaskList)
 }
 
-const TaskListProvider = ({children}:Props) =>{
+const TaskListProvider = ({ children }: Props) => {
 
- const initialState = localStorage.getItem('tasks')
- ? JSON.parse(localStorage.getItem('tasks') || '')
- : []
+    const initialState = localStorage.getItem('tasks')
+        ? JSON.parse(localStorage.getItem('tasks') || '')
+        : []
     const [isComplete, setIsComplete] = useState<boolean>(false);
-    const [allTasks, setAllTasks] =  useState<any>(initialState);
+    const [allTasks, setAllTasks] = useState<any>(initialState);
     const [editTask, setEditTask] = useState<TaskType>(initialState)
 
 
-  
 
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(allTasks))
-  }, [allTasks])
-    const handleDelete = (id:string) =>{
-    const filteredTasks = allTasks.filter((task:TaskType) => task.id !== id)
-    setAllTasks(filteredTasks)
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(allTasks))
+    }, [allTasks])
+    const handleDelete = (id: string) => {
+        const filteredTasks = allTasks.filter((task: TaskType) => task.id !== id)
+        setAllTasks(filteredTasks)
     }
-    const clearList =() =>{
+    const clearList = () => {
         setAllTasks([])
     }
-    const findTask= (id:string) =>{
-       const task = allTasks.find((task:TaskType) => task.id === id)
-       setEditTask(task)
+    const findTask = (id: string) => {
+        const task = allTasks.find((task: TaskType) => task.id === id)
+        setEditTask(task)
     }
-    const handleEdit = (formValues:TaskType) =>{
-    const {id, task,desc, time} = formValues
-    const newTasks = allTasks.map((todo:TaskType) =>todo.id === id ? {task, id,desc, time}: todo);
-    setAllTasks(newTasks)
+    const handleEdit = (formValues: TaskType) => {
+        const { id, task, desc, time } = formValues
+        const newTasks = allTasks.map((todo: TaskType) => todo.id === id ? { task, id, desc, time } : todo);
+        setAllTasks(newTasks)
     }
-   
-    return <TaskList.Provider value={{findTask, setEditTask,handleEdit, editTask, isComplete, setAllTasks, setIsComplete, allTasks, handleDelete, clearList}}>{children}</TaskList.Provider>
+
+    return <TaskList.Provider value={{ findTask, setEditTask, handleEdit, editTask, isComplete, setAllTasks, setIsComplete, allTasks, handleDelete, clearList }}>{children}</TaskList.Provider>
 
 }
 export default TaskListProvider
