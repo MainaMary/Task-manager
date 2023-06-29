27c7b7339552fb9fac1507be20b4ccc5514d2dc8
Mary <marieshix98@gmail.com>
2023-06-29 15:12:41 +0300

    add theme style for the toggle component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,40 @@
-import {IoMdNotifications} from "react-icons/io"
-import {MdDarkMode,MdOutlineLightMode} from"react-icons/md"
+import { IoMdNotifications } from "react-icons/io"
 import { useTaskList } from "../context/appContext"
+import ThemeToggle from "./ThemeToggle"
+import { AppTheme } from "../model/types"
 const Navbar = () => {
-  const {allTasks} = useTaskList()
+  const { allTasks, theme } = useTaskList()
+  const headerStyle: AppTheme = {
+    dark: {
+      background: 'black',
+      color: '#ffff'
+    },
+    light: {
+      background: '#eoeoeo',
+      color: 'black'
+    },
+    common: {
+      transition: 'all 1s ease'
+    }
+  }
+  const themeStyle = {
+    ...headerStyle.common,
+    ...(theme === "light" ? headerStyle.light : headerStyle.dark)
+  }
   return (
-    <div className="h-24   px-8 lg:px-16 flex justify-between  items-center bg-primary-color text-white text-md">
+    <nav style={themeStyle} className="h-24   px-8 lg:px-16 flex justify-between  items-center bg-primary-color text-white text-md">
       <h4 className="font-medium text-2xl">Task Manager</h4>
-      <div className="relative">
-      <IoMdNotifications size={32}/>
-      {allTasks.length > 0 ?  <p className=" top-[-16px] left-4 border-[2px] border-black w-[30px] h-[30px] rounded-full absolute flex items-center justify-center text-md font-medium">{allTasks.length}</p> :''}
-     
+      <div className="flex h-auto items-center gap-2">
+        <ThemeToggle />
+        <div className="relative">
+          <IoMdNotifications size={32} />
+          {allTasks.length > 0 ? <p className=" top-[-16px] left-4 border-[2px] border-black w-[30px] h-[30px] rounded-full absolute flex items-center justify-center text-md font-medium">{allTasks.length}</p> : ''}
+
+        </div>
       </div>
-      
-    </div>
+
+
+    </nav>
   )
 }
 
