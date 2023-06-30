61dc2d1a5141cf0a98dffaaeed7305308cf5d455
Mary <marieshix98@gmail.com>
2023-06-30 13:22:50 +0300

    toggle dark mode functionality

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,15 +1,28 @@
-import {MdDarkMode,MdOutlineLightMode} from"react-icons/md"
-import { useTaskList } from "../context/appContext"
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
+import { useTaskList } from "../context/appContext";
 const ThemeToggle = () => {
-const {setTheme, theme} = useTaskList()
-const handleThemeToggle = () =>{
-  setTheme(theme === "light" ? "dark":"light") 
-}
+  const { setTheme, theme } = useTaskList();
+  const handleThemeToggle = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  console.log(theme);
   return (
     <div onClick={handleThemeToggle}>
-        {theme === "light" ? <MdOutlineLightMode/> : <MdDarkMode/>}
+      <div className="flex h-auto items-center gap-1 cursor-pointer">
+        {theme === "light" ? (
+          <>
+            <MdDarkMode size={24} />
+            <p>dark</p>
+          </>
+        ) : (
+          <>
+            <MdOutlineLightMode size={24} />
+            <p>light</p>
+          </>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ThemeToggle
+export default ThemeToggle;
